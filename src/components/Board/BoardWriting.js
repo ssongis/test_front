d40993ef8f4fb528b100fac0c970ab8aebc7db15
ssongis <thddltmf97@naver.com>
2023-02-10 12:29:09 +0900

    게시판 등록+뷰+목록

--- src/components/Board/BoardWriting.js
+++ src/components/Board/BoardWriting.js
@@ -3,10 +3,57 @@ import { CKEditor } from '@ckeditor/ckeditor5-react';
 import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
 import styled from 'styled-components';
 import parse from 'html-react-parser';
+import { MainBlock } from '@/components/CommunityMain/main.styles';
+import Footer from '@/components/Footer';
+import Header from '@/components/Header/Header';
+import { CommonWrapper } from '@/styles/style';
+import Link from 'next/link';
 
-// const parse = require ('html-react-parser');
+export const BoardListWrapper = styled.main`
+hr{
+  margin: 0px;
+  padding: 0px;
+  height: 1px;
+  border: none;
+  background-color: rgb(234, 237, 239);
+}
 
-// parse({element.content})
+.item{
+  
+  position : relative;
+  width:100%;
+  height:250px;
+  padding:20px;
+}
+
+.title{
+  font-size:20px;
+  font-weight:bold;
+  position : relative;
+  width:100%;
+  margin-bottom:10px;
+}
+
+.content{
+  position : relative;
+  width:100%;
+  
+  
+  height:150px;
+}
+
+.nickname{
+  width:30%;
+  
+  float:left;
+}
+
+.regdate{
+  width:30%;
+  
+  float:left;
+}
+`
 
 export const BoardWrapper = styled.main`
 .BoardWriting {
@@ -50,7 +97,6 @@ export const BoardWrapper = styled.main`
 }
 `
 
-
 const BoardWriting = props => {
   // useState를 이용해 입력한 내용을 state에 저장
   const [content, setContent] = useState({
@@ -61,7 +107,10 @@ const BoardWriting = props => {
   // 스테이트에 저장된 내용 => 화면에 보여주기
   const [viewContent, setViewContent] = useState([]);
 
-  // 인풋 내용이 변할 때 값을 스테이트에 업데이트 해 주는 기능
+  // => 목록에 보여주기
+  const [listContent, setListContent] =useState([]);
+
+  // 인풋 내용이 변할 때 값을 뷰 스테이트에 업데이트 해 주는 기능
   const getValue = e => {
       const {name, value} = e.target;
       setContent({...content,
@@ -114,7 +163,35 @@ const BoardWriting = props => {
             }>글쓰기</button>
           </div>
         </div>
+        <BoardListWrapper>
+        <h1>게시판 목록</h1>
+        <hr/>
+        {/* 링크 게시판으로 이동하도록 바꾸기 */}
+            <Link href="/" legacyBehavior> 
+            <a>
+              <div className='item'>
+                <div className='title'>
+                  제목
+                </div>
+                <div className='content'>
+                  내용
+                </div>
+                <div className='nickname'>
+                  작성자
+                </div>
+                <div className='regdate'>
+                  작성날짜
+                </div>
+                <div className='reply'>
+                  댓글
+                </div>
+              </div>
+            </a>
+            </Link>
+            <hr/>
+        </BoardListWrapper>      
         </BoardWrapper>
+        
       );
   }
 
