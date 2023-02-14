40445dbecee421e3d4497cf195582ead704873b7
ssongis <thddltmf97@naver.com>
2023-02-14 16:51:31 +0900

    자유게시판 DB 미완

--- src/components/Board/BoardWriting.js
+++ src/components/Board/BoardWriting.js
@@ -1,50 +1,12 @@
 import {useState} from 'react';
 import { CKEditor } from '@ckeditor/ckeditor5-react';
 import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
-// import parse from 'html-react-parser';
+import parse from 'html-react-parser';
 import { BASE_URL, BOARD } from "../../config/host-config";
 
 export const BoardWriting = props => {
 
-  const API_BASE_URL = BASE_URL + BOARD;
-
-    const boardHandler = e => {
-        e.preventDefault();
-
-        // 제목 입력태그, 내용 입력태그
-        const $title = document.getElementById('title');
-        const $content = document.getElementById('content');
-
-        // 서버에 게시글 저장 요청
-        fetch(`${API_BASE_URL}/write`, {
-            method: 'POST',
-            headers: { 'content-type': 'application/json' },
-            body: JSON.stringify({
-                title: $title.value,
-                content: $content.value
-            })
-        })
-        .then(res => res.json())
-        .then(result => {
-            //console.log(result);
-            if (result.message) {
-                // 로그인 실패
-                alert(result.message);
-            } else {
-                alert('저장 성공!');
-
-                // 발급받은 토큰을 저장, 회원정보 저장
-                // 브라우저가 제공 로컬스토리지(브라우저가 종료되어도 남아있음)
-                // 세션스토리지(브라우저종료되면 사라짐)
-                localStorage.setItem('ACCESS_TOKEN', result.token);
-                localStorage.setItem('LOGIN_USERNAME', result.userName);
-
-                window.location.href='/BoardView';
-            }
-        });
-    };
-
-  // useState를 이용해 입력한 내용을 state에 저장
+  // 게시판 저장 데이터
   const [content, setContent] = useState({
     title: '',
     content: '',
@@ -61,9 +23,28 @@ export const BoardWriting = props => {
     console.log(content);
   };
 
+  // const handleInsert = () => {
+  //   fetch(`${API_BASE_URL}/write`, {
+  //     method: 'POST',
+  //     headers: { 'content-type': 'application/json' },
+  //     body: JSON.stringify(content)
+  // })
+  // .then(res => res.json())
+  // .then(result => {
+  //     //console.log(result);
+  //     if (result.message) {
+          
+  //         alert(result.message);
+  //     } else {
+  //         alert('저장 성공!');
+  //         window.location.href='/BoardList';
+  //     }
+  // });
+  // }
+
     return (
         <div className="BoardWriting">
-          {/* <div className='board-container'>
+          <div className='board-container'>
             {viewContent.map(element =>
               <div>
                 <h2>{element.title}</h2>
@@ -72,31 +53,40 @@ export const BoardWriting = props => {
                 </div>
               </div>
             )}  
-          </div>  */}
-          <form noValidate onSubmit={boardHandler}>
-          <div className='form-wrapper'>
+          </div> 
+          <div className='form-wrapper'>  
           <input className="title-input" type='text' placeholder='제목'
              onChange={getValue} name='title'/>
              <CKEditor
                editor={ClassicEditor}
-               onReady={editor => {
-               console.log('Editor is ready to use!', editor);
-          }}
+               config={{
+                placeholder: "글을 입력해보세요!",
+              }}
+              onReady={editor => {
+                console.log('Editor is ready to use!', editor);
+           }}
+ 
              onChange={(event, editor) => {
              const data = editor.getData();
-               console.log({ event, editor, data });
+        
+             console.log({ event, editor, data });
              setContent({
               ...content, content: data
              })
              console.log(content);
-          }} 
+            //  this.setState({
+            //  Question_Data:data});
+
+          }}
              onBlur={(event, editor) => {
-               console.log('Blur.', editor);
+             console.log('Blur.', editor);
           }}
              onFocus={(event, editor) => {
-               console.log('Focus.', editor);
-          }} name='content'
+             console.log('Focus.', editor);
+          }}
+          
         />
+      
           </div> 
           <div>
           {/* <a href="/BoardList"> */}
@@ -106,12 +96,9 @@ export const BoardWriting = props => {
             }
             }>작성</button>
           {/* </a> */}
-          <a href="/">
           <button className="submit-button" 
             onClick="href=/">취소</button>
-          </a>
           </div>
-          </form>
         </div>
       );
   }
